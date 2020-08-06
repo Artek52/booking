9106d82466ddf35f1c8730323ca713f39014d639
p7nt4 <giuseppepentangelo12@gmail.com>
2020-08-06 18:06:13 +0200

    fixed drawed table but it gets drawed 2 times

--- frontend/views/site/_disponibilitaDetails.php
+++ frontend/views/site/_disponibilitaDetails.php
@@ -3,42 +3,25 @@ use yii\helpers\Html;
 use yii\helpers\HtmlPurifier;
 ?>
 
-<!-- <div class="post">
-    <h2>Disp</h2>
-    <div class="row">
-        <?php ''//foreach ($model as $key => $value) : ?>
-            <?php''// if ('id' != $key) : ?>
-                <div class='col-lg-2'>
-                    <div class='well' style="background-color: <?= ''//($value ? '#33ff00' : '#ff0000') ?>">
-                        <p><?='' //str_replace('_', ':', str_replace('orario_', '', $key)) ?></p>
-                        <p><?=''// ($value ? 'X' : '-') ?></p>
-                    </div>
-                </div>
-            <?php'' //endif ; ?>
-        <?php''// endforeach ; ?>
-    </div>
-</div> -->
 
-
-<div class="row">
     <table class="table">
-        <?php for ($i=0; $i <=23 ; $i++) : ?>
-            <?php foreach (['00', '15', '30', '45'] as $y) : ?>
-                <?= $tmp = substr("0" . $i, -2) . ":" . $y; ?>
+
+
+        <thead>
+            <tr>
+             <?= "<th scope='row'> # </th>" ?>
+             <?= "<th scope='row'> risorsa1 </th>" ?>
+             <?= "<th scope='row'> risorsa2 </th>" ?>
+            </tr>
+         </thead>
+          <?php for ($i=0; $i <=23 ; $i++) :
+              foreach (['00', '15', '30', '45'] as $y) :
+              $tmp = substr("0" . $i, -2) . ":" . $y; ?>
   <tbody>
    <tr>
-     <th scope="row"> <?= $tmp ?> </th>
-     <td>Mark</td>
-     <td>Otto</td>
-     <td>@mdo</td>
+       <?= "<th scope='row'> $tmp </th>" ?>
    </tr>
- </tbody>
+    </tbody>
+<?php endforeach ; ?>
+<?php endfor ; ?>
 </table>
-
-
-            <div class="col-lg-2">
-
-            </div>
-        <?php endforeach ; ?>
-        <?php endfor ; ?>
-    </div>
--- frontend/views/site/viewDisponibilita.php
+++ frontend/views/site/viewDisponibilita.php
@@ -3,23 +3,10 @@ use yii\widgets\ListView;
 use yii\widgets\DetailView;
 ?>
 
-<?php
- // DetailView::widget([
- //   'model' => $model,
- //   // 'attributes' => [
- //   //   'data',
- //   // ],
- // ]);
- // foreach ($model->y as $y) {
- //}
-?>
-
-
 <?=
 ListView::widget([
     'dataProvider' => $disponibilitaProvider,
     'itemView' => '_disponibilitaDetails'
 ]);
-
 
 ?>
